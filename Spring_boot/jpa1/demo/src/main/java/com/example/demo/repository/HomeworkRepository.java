4c1fff92a7cc7d643f96a5edc1949576d5e6e757
Exynix <davids_barretom@javeriana.edu.co>
2023-10-14 12:39:35 -0500

    Revert "Reestructuracion de archivos"
    This reverts commit 3fa68f84fa882b57d5962b4e20f7ca4301a8ac05.

--- Spring_boot/jpa1/demo/src/main/java/com/example/demo/repository/HomeworkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Homework;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface HomeworkRepository extends JpaRepository<Homework, Long> {
-
-
-}
--- Spring_boot/jpa2/demo/src/main/java/com/example/demo/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-
-
-}
